1c4e2a2e3366f49da9e6c9e387645ad31e40e555
USER-ACER-PC <nicothecruzguardian@gmail.com>
2016-09-25 20:23:27 +0700

    Simple Data Binding Final

--- AdvanceWidget3/app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/advancewidget3/MainActivity.java
+++ AdvanceWidget3/app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2019/advancewidget3/MainActivity.java
@@ -1,8 +1,11 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2019.advancewidget3;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import id.sch.smktelkom_mlg.tugas01.xirpl2019.advancewidget3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     String nama;
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-       binding = DataBindingUtil.setContentView(R.layout.activity_main);
+       binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setNama(nama);
     }
 }
